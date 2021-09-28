76868ea6ca5a95a7c885e26504b507a01a2f42aa
MattyIce96 <88891996+MattyIce96@users.noreply.github.com>
2021-09-28 12:19:55 -0400

    Added config trailers spawns etc

--- config.lua
+++ config.lua
@@ -2,4 +2,20 @@ Config = {}
 
 Config.dashboard = vector3(-79.8, 6501.9, 31.5)
 
+-- Trucker should already have his own truck
+-- Trailers
+Config.CarTrailer = vector4(-97.44, 6495.43, 31.49, 230.36) -- trailer for cars
+Config.GunTrailer = vector4(-97.44, 6495.43, 31.49, 230.36) -- trailer for guns
+Config.ClothesTrailer = vector4(-97.44, 6495.43, 31.49, 230.36) -- trailer for clothing
+Config.FoodTrailer = vector4(-97.44, 6495.43, 31.49, 230.36) -- trailer for Food
+Config.GasTrailer = vector4(-97.44, 6495.43, 31.49, 230.36) -- trailer for Gas
+
+-- Vehicles
+Config.CarTrailer = vector4(-64.79, 6498.07, 31.49, 129.93) -- 1 spawn to avoid people stealing
+
+--Trailer Delivery Points
+Config.CarTrailer = vector4(-15.84, -1096.75, 26.67, 161.35) -- Where the cardealer will meet the trucker
+Config.GunTrailer = vector4(-8.13, -1110.64, 28.64, 159.49) -- Where the weapon dealer will meet the trucker
+
+
 Config.Marker = { enable = true, drawDist = 7.5, type = 20, scale = {x = 0.6, y = 0.6, z = 0.6}, color = {r = 255, g = 165, b = 0, a = 100}}
