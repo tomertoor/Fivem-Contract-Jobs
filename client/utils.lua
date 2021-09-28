e2a531605581f8579ba79d80365b15f8f6ca350d
MattyIce96 <88891996+MattyIce96@users.noreply.github.com>
2021-09-28 11:57:16 -0400

    Added Utils code, added startcarcontract & spawncartrailer for cardealer job

--- client/utils.lua
+++ client/utils.lua
@@ -13,3 +13,44 @@ function DrawText3Ds(x,y,z, text)
     local factor = (string.len(text)) / 500
     DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 0, 0, 0, 80)
 end
+
+function StartCarContract() -- cardealers comment
+    local StartContract = false
+    local BaseFee = 500
+    local FeePerExtraDelivery = 850
+
+    ESX.TriggerServerCallback("isTruckOwned", function(HasTruck)
+        local HasTruck = 't680'
+        local SpawnCarTrailer = 'tr2'
+        if HasTruck then
+            SpawnCarTrailer()
+        else
+            return
+            TriggerEvent('chat:addMessage', {
+                args = { 'You need your company truck!' }
+            })
+        end
+    end, source)
+end
+
+function SpawnCarTrailer() -- cardealers
+
+    if not IsModelInCdimage("tr2") or not IsModelAVehicle("tr2") then
+        return
+    end
+
+    -- load the model
+    RequestModel("tr2")
+
+    -- wait for the model to load
+    while not HasModelLoaded("tr2") do
+        Wait(500) -- often you'll also see Citizen.Wait
+    end
+    local pos = Config.CarTrailer
+    local vehicle = CreateVehicle("tr2", pos.x, pos.y, pos.z, pos.w, true, false)
+
+    TriggerEvent('chat:addMessage', {
+        args = { 'Nice, now go deliver the cars to the dealer.' }
+    })
+
+end
